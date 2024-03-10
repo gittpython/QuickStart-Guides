d0704a63002950c5d54879cd5aa6e2bd50a193c2
gittpython <98565041+gittpython@users.noreply.github.com>
2024-03-10 17:04:27 -0400

    Arranged Files

---
+++ Scratch_Files/scratch03-07.py
@@ -0,0 +1,14 @@
+# 3 Controlling Program Flow 
+# Logical comparisons
+a = 1
+b = 2
+c = 3
+
+if a > b:
+    print("a is geater than b")
+    if b != c:
+        print ("B is not equal to c")
+    else:
+        print("b is equal c")
+else:
+    print("a is less than b")
---
+++ Scratch_Files/scratch03-11.py
@@ -0,0 +1,9 @@
+# 3 Display planets
+# Loops
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+for planet in planets:
+    print(planet)
+
+# %%
+    print("hello world")
+# %%
