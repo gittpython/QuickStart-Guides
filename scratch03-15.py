29b0c31f6a64938446b5e8e43a49b13ec96d1afe
gittpython <98565041+gittpython@users.noreply.github.com>
2024-03-13 00:47:40 -0400

    Another section

---
+++ scratch03-15.py
@@ -0,0 +1,11 @@
+# the planets
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+# Display the plant and its number
+for index, value in enumerate(planets):
+    print("Plnet " + str(index) + ": " + value)
+print("=========================")
+print("Abs for the the adjusted\n")
+# Display the plant and its real number
+for index, value in enumerate(planets):
+    print("Plnet " + str(index + 1) + ": " + value)
