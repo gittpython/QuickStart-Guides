f7dcc56d34120e452d41e1f10a208012d6304785
gittpython <98565041+gittpython@users.noreply.github.com>
2024-03-16 00:56:12 -0400

    ghe

--- cups.py
+++ cups.py
@@ -1,4 +1,4 @@
 price = input("What is the price of a cup of coffee? " )
 cups = input("How many cups do you want? ")
 total = float(price) * int(cups)
-print("Your total is $"+ str(total) + " for "+ cups+ " cups.")
+print("Your total is $"+ str(total) + " for "+ cups+ " cupss.")
