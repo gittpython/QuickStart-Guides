a1299885a038746f4416b863b44131f51f8ee85e
gittpython <98565041+gittpython@users.noreply.github.com>
2024-03-21 01:48:57 -0400

    Loops

---
+++ Coffee Program/CCS-03.py
@@ -0,0 +1,99 @@
+#ClydeBank Coffee Shop Simulator 4000
+#Copyright 2024 (c) ClydeBank Media, All Rights Reserved.
+# Impoer items from the random module to generate weather
+
+from random import seed
+from random import randint
+
+# Current day number
+day = 1
+
+# Starting cash on hand
+cash = 100.00
+
+# Coffee on hand (cups)
+coffee = 100
+
+#Sales list of disctionaries
+#sales = [
+    # {
+    #     "day": 1.
+    #     "coffee_inv": 100,
+    #     "advertising": "10",
+    #     "temp": 68,
+    #     "cups_sold": 16
+    # },
+   # {
+    #     "day": 2.
+    #     "coffee_inv": 84,
+    #     "advertising": "15",
+    #     "temp": 72,
+    #     "cups_sold": 20
+    # },
+   # {
+    #     "day": 3.
+    #     "coffee_inv": 64,
+    #     "advertising": "5",
+    #     "temp": 78,
+    #     "cups_sold": 10
+    # },
+#]
+
+# Create an empty sales list
+sales=[]
+
+#print Welcome message
+print("ClydeBank Coffee Shop Simulator 4000, Version 1.00")
+print("Copyright 2024 (c) ClydeBank Media, All Rights Reserved.\n")
+print("Let's collect some information before we start the game.\n")
+
+# Get name and shop name
+name = input("What is your name? ")
+shop_name = input("What do you want to name your coffee shop? ")
+
+print("\nOK, let's get started. Have fun")
+
+# The main game loop
+running = True
+while running:
+    # Display the day and add a "fancy" text effect
+    print("\n------| Day " + str(day) + " @ " + shop_name + " |-----")
+    
+    #Generate  a random temperature between 20 and 90
+    # We will consider seasons later on, but this is good enough for now
+    temperature = randint(20, 90)
+    
+    # Display the cash and weather
+    print("You have $" + str(cash) + "cash and it's " + str(temperature) + " degrees.")
+    print("You have coffee on hand  to make " + str(coffee) + " cups.\n")
+    
+    # Get price os a cup of coffee
+    cup_price = input("What do you want to charge per cup of coffee? ")
+    
+    # Get price of a cup of coffee
+    print("\n You can buy advertising to help promote sales ")
+    advertising = input("How much advertising do you want to buy? (0 for none)? ")
+    
+    # Convert advertising into float
+    advertising = float(advertising)
+    
+    # Deduct advertising from cash on hand
+    cash -= advertising
+    
+    # TODO calculate today's performance
+    # TODO Display today's performane
+    
+    # Before we loop around, add a day
+    day += 1
+    
+
+    
+print("\nThanks, " + name + ". Let’s set some initial pricing.\n")
+
+# Get initial price of a cup of coffee
+cup_price = input("What do you wnat to charge per cup of coffee? ")
+
+#Display what we have
+print("\nGreat. Here's what we've collected do far.\n")
+print("Your name is " + name + "and you're opening " + shop_name + "!")
+print("Your first cup of coffee will sell for $" + str(cup_price) + " Wow !!!.\n")
--- Random Num Game.py
+++ Random Num Game.py
@@ -2,18 +2,25 @@
 # 
 from random import seed
 from random import randint
-number = randint(1, 10)
+number = randint(1, 9)
 
 print("This is a ramdom number game")
-print("Simple to follow along")
-usrInput = input("Pick  number between 1 and 10: ")
- #verify the number
-if usrInput >= 11:
-     print("you idiot, cant you read? Try again\n")
-elif usrInput <= 0:
-     print("You are trying me...")
-print("So you selected " + usrInput)
-print("it is not")
 
-     
-     
+print("Simple to follow along, pick a number: ")
+
+upick = input("What is your pic")
+while upick == number:
+     print("Good Guess")
+if int(upick != number):
+     print("The number is" + " " + str(number))
+     if int(upick) == number:
+          print("YOU WON")
+     elif int(upick) < number:
+          print("lower")
+     elif int(upick) > number:
+          print("higher")
+     else:
+          upick = False
+
+     print("helloooooo")
+     # upick = input("guess again: ")
