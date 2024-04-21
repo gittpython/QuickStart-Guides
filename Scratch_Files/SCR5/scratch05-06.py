0f759b5ddaff1f2db41deb2607e34800e09e846d
gittpython <98565041+gittpython@users.noreply.github.com>
2024-04-21 17:35:23 -0400

    d

---
+++ Scratch_Files/SCR5/scratch05-06.py
@@ -0,0 +1,11 @@
+# Define the average funtion
+def average(*numbers):
+    sum = 0
+    for n in numbers:
+        # Addn to sum
+        # (+= means add n to sum and store in sum)
+        sum += n
+        print("The sum now is " + str(sum))
+    return sum / len(numbers)
+# Use yiur newly minted function
+print("The average is: " + str(average(10, 40, 80, 74, 16, 42, 12, 6,)) )   
