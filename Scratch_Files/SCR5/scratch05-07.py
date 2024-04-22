7cced8f0ddf57971f5e3ce985e3bfdc7c0370524
gittpython <98565041+gittpython@users.noreply.github.com>
2024-04-21 21:49:40 -0400

    debug

---
+++ Scratch_Files/SCR5/scratch05-07.py
@@ -0,0 +1,14 @@
+# 99 Bottles
+
+# While loop
+bottles = 99
+while bottles >0:
+    print(str(bottles) + " bottles of beer in te wall.")
+    print(str(bottles) + " bottles of beer.")
+    bottles -= 1
+    print("Take one down, pass it around,")
+    print(str(bottles) + " bottles of beer on the wall.\n")
+    
+    
+
+          
